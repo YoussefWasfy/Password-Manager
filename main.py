d1ee5a0da22a8f5b4cbb92e4eb9ef29a3731b468
YOUSSEF WASFY <ahmedwasfy@Wasfys-Air.local>
2021-02-12 17:00:32 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,117 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+import json
+
+
+# -----------------------------Search Function------------------------------------#
+def search_website():
+    website = website_entry.get()
+    email = ''
+    password = ''
+    try:
+        data_file = open('data.json')
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message='No Data File Found.')
+    else:
+        data = json.load(data_file)
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title=website, message=f'Email:{email}\nPassword:{password}')
+            data_file.close()
+        else:
+                messagebox.showinfo(title='Not Found', message='This website has not been registered yet')
+
+
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for char in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for char in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for char in range(nr_numbers)]
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_data():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {website: {
+        'email': email,
+        'password': password
+    }}
+    if website == '' or email == '' or password == '':
+        messagebox.showerror(title='Error', message='Do not leave empty Entries')
+        website_entry.focus
+    else:
+        is_okay = messagebox.askokcancel(title=website_entry.get(), message=f'These are the details entered:\n'
+                                                                            f'Email:{email_entry.get()}\n'
+                                                                            f'Password:{password_entry.get()}\n')
+        if is_okay:
+            try:
+                with open('data.json', 'r') as data_file:
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open('data.json', 'w') as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                data.update(new_data)
+                with open('data.json', 'w') as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+                website_entry.focus()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Password Manager')
+window.config(padx=20, pady=20)
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+website_label = Label(text='Website:')
+website_label.grid(row=1, column=0)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
+website_entry.focus()
+search_button = Button(text='Search', width=13, command=search_website)
+search_button.grid(row=1, column=2)
+email_label = Label(text='Email/Username:')
+email_label.grid(row=2, column=0)
+email_entry = Entry(width=35)
+email_entry.insert(0,#'Add your email here to start with it')
+email_entry.grid(row=2, column=1, columnspan=2)
+password_label = Label(text='Password:')
+password_label.grid(row=3, column=0)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+generate_button = Button(text='Generate', width=13, command=generate_password)
+generate_button.grid(row=3, column=2)
+add_button = Button(text='Add', width=36, command=save_data)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
